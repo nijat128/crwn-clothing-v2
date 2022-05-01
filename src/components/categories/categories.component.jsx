1a7333ecf6b05ff2287e06991c7221aa7bd18d29
ML\re86 <nijat.a.abdulkarimli@sunlife.com>
2022-05-01 00:40:19 -0400

    Added directory component for different categories

---
+++ src/components/categories/categories.component.jsx
@@ -0,0 +1,17 @@
+import CategoryItem from "../category-item/category-item.component";
+import '../categories/categories.styles.scss';
+
+
+function Categories({categories}) {
+    
+    return(
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+    );
+}
+
+
+export default Categories;
